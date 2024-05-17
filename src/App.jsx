4e9dbc214d53a723bc31ad09c63405f4e99c7574
Amit Gavali <shambho@Amits-MacBook-Air.local>
2024-05-18 03:14:13 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+import Guide from './components/Guide';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Guide />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../assets/taksh logo white.svg';
+
+const NavBar = () => {
+    return (
+        <nav className="navbar bg-dark">
+            <div className="container">
+                <a className="navbar-brand" href="#">
+                    <img src={logo} alt="Taksh-Logo" width="150" height="50" />
+                </a>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
