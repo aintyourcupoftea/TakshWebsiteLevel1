d253fda68685d4107ac403595201ccc6c45af4ea
Amit Gavali <shambho@Amits-Air.comnect.local>
2024-05-19 03:45:11 +0530

    updated message for opening box and created second message

---
+++ src/components/CountDown.jsx
@@ -0,0 +1,13 @@
+import Badge from 'react-bootstrap/Badge';
+
+function CountDown() {
+    return (
+        <div>
+            <h1>
+                You need<Badge bg="secondary">New</Badge>
+            </h1>
+        </div>
+    )
+}
+
+export default CountDown
