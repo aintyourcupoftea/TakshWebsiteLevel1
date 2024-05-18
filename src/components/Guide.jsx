13f8a968d8f289a77cb9f57d186b14a7db23c087
Amit Gavali <shambho@Amits-MacBook-Air.local>
2024-05-18 12:11:10 +0530

    Added animations to lottie

--- src/components/Guide.jsx
+++ src/components/Guide.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import Lottie from "lottie-react";
 import chief from "../assets/guide.json";
+import '../styles/guide.css'; // Import the CSS file
 
 const Guide = () => {
     const style = {
-        width: "35%",
-        height: "35%",
+        width: "100%",
+        height: "auto",
+        maxWidth: "400px", // Adjust max-width as needed
         margin: "auto",
     };
 
     return (
-        <div className="d-flex justify-content-center align-items-center">
-            <Lottie animationData={chief} loop={true} autoplay={true} style={style} />
+        <div className="guide-container">
+            <div className="lottie-container slide-fade-in-animation">
+                <Lottie animationData={chief} loop={true} autoplay={true} style={style} />
+            </div>
+            <div className="another-component">
+                {/* Placeholder for another React component */}
+            </div>
         </div>
     );
 };
