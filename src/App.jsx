69fe0afc9e634f6ce6e7378eeda195c837b53492
Amit Gavali <shambho@Amits-MacBook-Air.local>
2024-05-20 19:40:09 +0530

    updated footer ui

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import NavBar from './components/NavBar';
 import Guide from './components/Guide';
 import Footer from './components/Footer';
 
+import './App.css'; // Import your main CSS file where the Flexbox rules are defined
 
 function App() {
   return (
-    <>
+    <div className="page-container"> {/* Add the page container */}
       <NavBar />
-      <Guide />
+      <div className="content-wrapper"> {/* Add content wrapper */}
+        <Guide />
+      </div>
       <Footer />
-    </>
+    </div>
   );
 }
 
 export default App;
-
